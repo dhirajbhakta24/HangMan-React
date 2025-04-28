33d935461c87c0c75d77fd869bad0aa5b77e495e
Dhiraj Kumar Bhakta <bhaktadhiraj@gmail.com>
2025-04-28 23:49:38 +0530

    added codes

--- src/components/TextInputForm/TextInputForm.jsx
+++ src/components/TextInputForm/TextInputForm.jsx
@@ -1,43 +1,45 @@
 //here how our component will look is defined , logic is implemented
-//in other container file which contain only logic 
+//in other container file which contain only logic
 // this type of segregation helps to follow the important rule in coding world
-// SRP -Single responsibility principle: 
-//SRP is clean code management technique which defines that each file 
-//should handle only one task or logic other things must be implemented 
-// 
+// SRP -Single responsibility principle:
+//SRP is clean code management technique which defines that each file
+//should handle only one task or logic other things must be implemented
+//
 
-import Button from '../Button/Button';
-import TextInput from '../TextInput/TextInput';
+import Button from "../Button/Button";
 
-function TextInputForm({inputType , handleFormSubmit , handleTextInputChange , 
-    handleShowHideClick
-}){
-    return(
-        <form onSubmit={handleFormSubmit}>
-            <div>
-                <TextInput
-                    type={inputType}
-                    label={'enter a phrase or word here'}
-                    placeholder={'Enter a phrase or word here ..'}
-                    onChangeHandler={handleTextInputChange}
-                />
-            </div>
-            <div>
-                <Button styleType="warning"
-                    text = {inputType === "password" ? "Show" : "Hide"}
-                    onClickHandler={handleShowHideClick}
-                />
-            </div>
-            <div>
-                <Button
-                    type="submit"
-                    styleType = "primary"
-                    text = "Submit"
-                />
-            </div>
+import TextInput from "../TextInput/TextInput";
 
-        </form>
-    );
+function TextInputForm({
+  inputType,
+  handleFormSubmit,
+  handleTextInputChange,
+  handleShowHideClick,
+}) {
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <div>
+        <TextInput
+          type={inputType}
+          label="Enter a word or a phrase"
+          placeholder="Enter a word or phrase here ..."
+          onChangeHandler={handleTextInputChange}
+        />
+      </div>
+
+      <div>
+        <Button
+          styleType="warning"
+          text={inputType === "password" ? "Show" : "Hide"}
+          onClickHandler={handleShowHideClick}
+        />
+      </div>
+
+      <div>
+        <Button type="submit" styleType="primary" text="Submit" />
+      </div>
+    </form>
+  );
 }
-export default TextInputForm;
 
+export default TextInputForm;
--- src/components/TextInputForm/TextInputFormContainer.jsx
+++ src/components/TextInputForm/TextInputFormContainer.jsx
@@ -1,40 +1,43 @@
-// this is the container and its contains only logic 
+// this is the container and its contains only logic
 import { useState } from "react";
+
 import TextInputForm from "./TextInputForm";
 
-function TextInputFormContainer(){
-    const [inputType , setInputType] = useState("password");
+function TextInputFormContainer() {
+  const [inputType, setInputType] = useState("password");
 
-    function handleFormSubmit(event){
-        event.preventDefault();
-        console.log("form submitted");
+  function handleFormSubmit(event) {
+    event.preventDefault();
+
+    console.log("Form submitted");
+  }
+
+  function handleTextInputChange(event) {
+    console.log("Text input changed");
+
+    console.log(event.target.value);
+  }
+
+  function handleShowHideClick() {
+    console.log("Show/Hide button clicked");
+
+    if (inputType === "password") {
+      setInputType("text");
+    } else {
+      setInputType("password");
     }
 
-    function handleTextInputChange(event){
-        console.log("Text Input Change");
-        console.log(event.target.value);
-    }
-    function handleShowHideClick(){
-        console.log("show/hide button clicked");
-        if(inputType === "password"){
-            setInputType("text");
-        }
-        else{
-            setInputType("password");
-        }
-        console.log(inputType);
-    }
+    console.log(inputType);
+  }
 
-    return(
+  return (
     <TextInputForm
-        inputType={inputType}
-        handleFormSubmit={handleFormSubmit}
-        handleTextInputChange={handleTextInputChange}
-        handleShowHideClick={handleShowHideClick}
+      inputType={inputType}
+      handleFormSubmit={handleFormSubmit}
+      handleTextInputChange={handleTextInputChange}
+      handleShowHideClick={handleShowHideClick}
     />
-
-    );
-
-
+  );
 }
+
 export default TextInputFormContainer;
