99f598ed919bb1d88cb097c101b4ebe83dd9e9db
Dhiraj Kumar Bhakta <bhaktadhiraj@gmail.com>
2025-05-03 15:11:42 +0530

    added react router dom

--- src/components/TextInputForm/TextInputFormContainer.jsx
+++ src/components/TextInputForm/TextInputFormContainer.jsx
@@ -2,15 +2,22 @@
 import { useState } from "react";
 
 import TextInputForm from "./TextInputForm";
+import { useNavigate } from "react-router-dom";
 
 function TextInputFormContainer() {
   const [inputType, setInputType] = useState("password");
   const [value , setValue] =useState("");
-
+  const navigate = useNavigate(); //useNavigate is a hook that returns
+  //a navigate function which helps to navigate to other page after checking 
+  //some conditions its is similar to Link of react router dom but provide capability
+  //to apply conditions
   function handleFormSubmit(event) {
     event.preventDefault();
 
     console.log("Form submitted" , value);
+    if(value){
+      navigate("/play")
+    }
   }
 
   function handleTextInputChange(event) {
--- src/pages/PlayGame.jsx
+++ src/pages/PlayGame.jsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
 function PlayGame(){
     return(
         <>
             <h1>PlayGame</h1>
+            <Link to ='/start' className ="text-blue-400">StartGame-Link</Link>
         </>
     );
 }
