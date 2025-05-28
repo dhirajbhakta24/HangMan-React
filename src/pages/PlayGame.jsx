7ac55a950d143216813230fd47a9c6f3cd1bd8c4
Dhiraj Kumar Bhakta <bhaktadhiraj@gmail.com>
2025-05-28 12:45:35 +0530

    added codes

--- src/pages/PlayGame.jsx
+++ src/pages/PlayGame.jsx
@@ -8,11 +8,16 @@ function PlayGame(){
    //<Link to="/message/hello/123">Go to Message</Link> it is passed through Link 
    //from where we are redirecting and it is only available to that page that
    //link is directing 
+   //rendering list
+   //    const arr =['hello','world']
+ //{/* {arr.map(Element=><h1>Element</h1>)} */}
 
-   const {state} = useLocation();
+     const {state} = useLocation();
     return(
-        <>
+        <>  
+            
             <h1>PlayGame {state.wordSelected}</h1>
+           
             <MaskedText text ={state.wordSelected} guessedLetters ={[]}/>
             <Link to ='/start' className ="text-blue-400">StartGame-Link</Link>
         </>
